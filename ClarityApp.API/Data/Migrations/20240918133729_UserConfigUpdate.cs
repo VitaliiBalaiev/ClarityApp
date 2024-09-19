a3f7233ab3846685bb99df99c84b56eabbd5d0dd
insxmniahauntsme <vlad.radyuk.2019@gmail.com>
2024-09-19 14:40:17 +0300

    Add migration files

---
+++ ClarityApp.API/Data/Migrations/20240918133729_UserConfigUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClarityApp.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserConfigUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnline",
+                table: "AppUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnline",
+                table: "AppUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
